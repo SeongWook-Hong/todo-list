78cd8e1fcd937b7172591b783a54b6a4d1bfbb76
hong <hsw5971@naver.com>
2024-08-15 23:57:53 +0900

    feat: signin, signup 페이지 제작

---
+++ src/pages/auth/signin/index.tsx
@@ -0,0 +1,63 @@
+import Button from '@/components/common/Button';
+import InputForm from '@/components/common/InputForm';
+import { usePostUser } from '@/pages/api/hooks/useUsers';
+import { useState } from 'react';
+
+const Signin = () => {
+  const { mutate: postUser } = usePostUser();
+
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+  });
+  const handleAddUser = (userInfo: { email: string; password: string }) => {
+    postUser(userInfo);
+  };
+  function handleChangeInput(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  }
+  function handleSubmitForm(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+    handleAddUser({ email, password });
+
+    console.log('Email:', email);
+    console.log('Password:', password);
+  }
+
+  return (
+    <>
+      <h1 className="mx-auto my-5">로그인</h1>
+      <form onSubmit={handleSubmitForm}>
+        <InputForm
+          htmlFor="email"
+          value={values.email}
+          onChange={handleChangeInput}
+        >
+          이메일
+        </InputForm>
+        <InputForm
+          htmlFor="password"
+          value={values.password}
+          onChange={handleChangeInput}
+        >
+          비밀번호
+        </InputForm>
+
+        <Button btn_type="primary" extraStyle="w-[100%] mt-2">
+          로그인
+        </Button>
+      </form>
+    </>
+  );
+};
+
+export default Signin;
---
+++ src/pages/auth/signup/index.tsx
@@ -0,0 +1,88 @@
+import Button from '@/components/common/Button';
+import InputForm from '@/components/common/InputForm';
+import { useGetUser, usePostUser } from '@/pages/api/hooks/useUsers';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+
+const Signup = () => {
+  const router = useRouter();
+
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+    retype: '',
+  });
+
+  const { mutate: postUser } = usePostUser();
+  const { mutate: getUser } = useGetUser();
+
+  const handleAddUser = (userInfo: { email: string; password: string }) => {
+    postUser(userInfo);
+  };
+  function handleChangeInput(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmitForm(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+    const retype = formData.get('retype') as string;
+
+    if (password !== retype) {
+      alert('패스워드가 일치하지 않습니다.');
+      return;
+    }
+    getUser(email, {
+      onSuccess: () => {
+        alert('이미 사용 중인 이메일 입니다.');
+        return;
+      },
+    });
+
+    handleAddUser({ email, password });
+    router.push('/auth/signin');
+  }
+
+  return (
+    <>
+      <h1 className="mx-auto my-5">로그인</h1>
+      <form onSubmit={handleSubmitForm}>
+        <InputForm
+          htmlFor="email"
+          value={values.email}
+          onChange={handleChangeInput}
+        >
+          이메일
+        </InputForm>
+        <InputForm
+          htmlFor="password"
+          value={values.password}
+          onChange={handleChangeInput}
+        >
+          비밀번호
+        </InputForm>
+        <InputForm
+          htmlFor="retype"
+          type={'password'}
+          value={values.retype}
+          onChange={handleChangeInput}
+        >
+          비밀번호 확인
+        </InputForm>
+        <Button btn_type="primary" extraStyle="w-[100%] mt-2">
+          회원가입
+        </Button>
+      </form>
+    </>
+  );
+};
+
+export default Signup;
