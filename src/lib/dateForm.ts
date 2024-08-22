b78a84e97def51389ad43ccc8550ae6352cef69a
hong <hsw5971@naver.com>
2024-08-22 20:53:19 +0900

    chore: 로직 변경

--- src/lib/dateForm.ts
+++ src/lib/dateForm.ts
@@ -1,10 +1,12 @@
 const YYYYMMDD = (date: Date) => date.toISOString().split('T')[0];
 
-const koreanDateString = (date: Date) =>
-  date.toLocaleDateString('ko-KR', {
+const koreanDateString = () => {
+  const date = new Date();
+  return date.toLocaleDateString('ko-KR', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
   });
+};
 
 export { YYYYMMDD, koreanDateString };
