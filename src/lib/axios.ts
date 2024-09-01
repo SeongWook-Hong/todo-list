4a89af1907fbb7caf015451dc3b8e7db0537ac8d
hong <hsw5971@naver.com>
2024-09-01 21:49:27 +0900

    chore: base url 수정

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
-const baseAxios = axios.create({ baseURL: '/api/', withCredentials: true });
+const baseAxios = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URI,
+  withCredentials: true,
+});
 
 export default baseAxios;
